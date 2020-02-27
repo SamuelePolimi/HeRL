d5bd5f1fe955738bd9395a5721a54d708f425187
Samuele <samuele@marconi.ias.informatik.tu-darmstadt.de>
2020-02-27 11:39:51 +0100

    neural network actor in HeRL

---
+++ herl/actor.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+import torch
+from herl.rl_interface import RLTask, RLAgent
+from herl.config import torch_type
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, h_layers, act_functions, rl_task, output_function=None):
+        """
+
+        :param h_layers: Each entry represents the no of neurons in the corresponding hidden layer.
+        :type h_layers: list
+        :param act_functions: list of activation functions for each input layer. len = len(layers) - 2
+        :type act_functions: list
+        :param output_function: Function to be applied to output layer. None if output is linear.
+        :type output_function: builtin_function
+        :param rl_task: Used to infer input_dim and output_dim
+        :type rl_task: RLTask
+
+        """
+
+        super(NeuralNetwork, self).__init__()
+        self.input_dim = rl_task.environment.state_dim
+        self.hidden = nn.ModuleList([nn.Linear(self.input_dim, h_layers[0])])
+        self.output_dim = rl_task.environment.action_dim
+
+        for input_size, output_size in zip(h_layers, h_layers[1:]):
+            self.hidden.append(nn.Linear(input_size, output_size))
+
+        self.hidden.append(nn.Linear(h_layers[-1], self.output_dim))
+        self.hidden = self.hidden.to(dtype=torch_type)
+
+        for layer in self.hidden:
+            nn.init.xavier_uniform_(layer.weight.data)
+            nn.init.xavier_uniform_(layer.bias.data[None])
+
+        self.act_functions = act_functions
+        self.out_function = output_function
+
+    def forward(self, x):
+        """
+
+        :param x:  Input feature
+        :type x: tensor
+        :return: Tensor of shape len(x)*output_dim
+        """
+        x = x.to(dtype=torch_type)
+        l_network = len(self.hidden)
+        for (i, linear_transform) in list(zip(range(l_network), self.hidden))[:-1]:  # still searching for a method to avoid loops
+            x = self.act_functions[i](linear_transform(x))
+        if self.out_function is None:
+            x = self.hidden[-1](x)
+        else:
+            x = self.out_function(self.hidden[-1](x))
+        return x
+
+
+class Actor(RLAgent):
+
+    def __init__(self, h_layers, act_functions, rl_task, output_function=None):
+        RLAgent.__init__(self)
+        self.nn = NeuralNetwork(h_layers, act_functions, rl_task, output_function)
+
+    def __call__(self, state):
+        return self.nn(state)
+
+    def get_action(self, state):
+        return self.nn(torch.tensor(state, dtype=torch_type)).detach().numpy()
--- herl/rl_debug.py
+++ herl/rl_debug.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
+import torch.nn
 
 from herl.rl_analysis import Analyser
 from herl.dataset import Dataset, Domain, Variable
 from herl.rl_interface import Critic, RLTask
-from herl.utils import ConstantPolicyPendulum, Pendulum2D, RandomPolicyPendulum
+from herl.utils import ConstantPolicyPendulum, Pendulum2D, RandomPolicyPendulum, MC2DPendulum, \
+    RLUniformCollector2DPendulum
 from herl.datasets.library import search
-from core.actor import Actor
+from herl.actor import Actor
 
-class PolicyEvaluationPendulum2D(Analyser):
 
-    def __init__(self, critic_class, dataset=None, policy=None, verbose=True, plot=True):
+class PolicyEvaluation(Analyser):
+
+    def __init__(self, critic_class, rl_task, dataset=None, policy=None,
+                 verbose=True, plot=True):
 
         Analyser.__init__(self, verbose, plot)
         self.critic_class = critic_class
 
-        self.rl_task = RLTask(Pendulum2D(), gamma=0.99, max_episode_length=200)
-        self.policy = Actor([50], [torch.relu], self.rl_task, lambda x: 2.0 * torch.tanh(x))
+        self.rl_task = rl_task
+        self.policy = policy
         self.dataset = dataset
         self.rl_algorithm = self.critic_class(self.rl_task, self.policy)  # type: Critic
 
@@ -58,42 +61,66 @@ class PolicyEvaluationPendulum2D(Analyser):
         return results
 
 
-class PolicyEvaluationPendulum2DGridConstant(PolicyEvaluationPendulum2D):
+class PolicyEvaluationPendulum2DGridConstant(PolicyEvaluation):
 
     def __init__(self, critic_class, verbose=True, plot=True):
 
         dataset = search(Domain(Variable("state", 2), Variable("value", 1)),
                               "pendulum2d", "grid", "constant", "0", "0.95", "value")
 
-        PolicyEvaluationPendulum2D.__init__(self, critic_class, dataset=dataset, policy=ConstantPolicyPendulum(),
+        PolicyEvaluation.__init__(self, critic_class, RLTask(Pendulum2D()), dataset=dataset, policy=ConstantPolicyPendulum(),
                                             verbose=verbose, plot=plot)
 
 
-class PolicyEvaluationPendulum2DConstant(PolicyEvaluationPendulum2D):
+class PolicyEvaluationPendulum2DConstant(PolicyEvaluation):
 
     def __init__(self, critic_class, verbose=True, plot=True):
         dataset = search(Domain(Variable("state", 2), Variable("value", 1)),
                          "pendulum2d", "sample_policy", "constant", "0", "0.95", "value")
 
-        PolicyEvaluationPendulum2D.__init__(self, critic_class, dataset=dataset, policy=ConstantPolicyPendulum(),
+        PolicyEvaluation.__init__(self, critic_class, LTask(Pendulum2D(), 0.95, 200), dataset=dataset, policy=ConstantPolicyPendulum(),
                                             verbose=verbose, plot=False)
 
 
-class PolicyEvaluationPendulum2DUniform(PolicyEvaluationPendulum2D):
+class PolicyEvaluationPendulum2DUniform(PolicyEvaluation):
 
     def __init__(self, critic_class, verbose=True, plot=True):
         dataset = search(Domain(Variable("state", 2), Variable("value", 1)),
                          "pendulum2d", "sample_policy", "uniform", "0.95", "value")
 
-        PolicyEvaluationPendulum2D.__init__(self, critic_class, dataset=dataset, policy=RandomPolicyPendulum(),
+        PolicyEvaluation.__init__(self, critic_class, RLTask(Pendulum2D(), 0.95, 200), dataset=dataset, policy=RandomPolicyPendulum(),
                                             verbose=verbose, plot=False)
 
 
-class PolicyEvaluationPendulum2DGridUniform(PolicyEvaluationPendulum2D):
+class PolicyEvaluationPendulum2DGridUniform(PolicyEvaluation):
 
     def __init__(self, critic_class, verbose=True, plot=True):
         dataset = search(Domain(Variable("state", 2), Variable("value", 1)),
                          "pendulum2d", "grid", "uniform", "0.95", "value")
 
-        PolicyEvaluationPendulum2D.__init__(self, critic_class, dataset=dataset, policy=RandomPolicyPendulum(),
+        PolicyEvaluation.__init__(self, critic_class, RLTask(Pendulum2D(), 0.95, 200), dataset=dataset, policy=RandomPolicyPendulum(),
+                                            verbose=verbose, plot=plot)
+
+
+class NeuralNetworkPolicyEvaluationPendulum2DGridUniform(PolicyEvaluation):
+
+    def __init__(self, critic_class, verbose=True, plot=True):
+
+        pendulum = Pendulum2D(initial_state=np.array([-np.pi, 0.]))
+        rl_task = RLTask(pendulum, 0.95, 200)
+        policy = Actor([50], [torch.nn.functional.relu], rl_task=rl_task)
+        ds = Dataset(Domain(Variable("state", 2)))
+        angle = np.linspace(-np.pi, np.pi, 100)
+        velocity = np.linspace(-8., 8., 100)
+        X, Y = np.meshgrid(angle, velocity)
+        x = X.reshape(-1, 1)
+        y = Y.reshape(-1, 1)
+        states = np.concatenate([x, y], axis=1)
+
+        ds.notify_batch(state=states)
+        mc_pendulum = MC2DPendulum(policy, ds, max_episodes_length=1000, pendulum=pendulum)
+        estimate = mc_pendulum.get_v_dataset(1)
+
+        PolicyEvaluation.__init__(self, critic_class, rl_task,
+                                  dataset=estimate, policy=policy,
                                             verbose=verbose, plot=plot)
